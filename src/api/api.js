5d270017508c22848c66d411b15a73963f5abe40
tundeadekunles1 <tundeadekunles1@outlook.com>
2025-05-18 15:05:59 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://newskill-bridge.vercel.app/api",
+  baseURL: "https://skillexbackend-5.onrender.com",
 });
 
 export default api;
