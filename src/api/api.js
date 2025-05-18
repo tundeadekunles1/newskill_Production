7318cb9bcc1c18cf23e8fc33144123e8e06c7bdd
tundeadekunles1 <tundeadekunles1@outlook.com>
2025-05-18 15:19:16 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skillexbackend-5.onrender.com",
+  baseURL: "https://skillexbackend-5.onrender.com/api",
 });
 
 export default api;
